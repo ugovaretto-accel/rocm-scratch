4dd4e39274050f0255b72730bb65e2d31d40e8c0
Ugo Varetto <ugovaretto@gmail.com>
2021-02-18 15:34:40 +0800

    Formatted

--- block-matrix-mul.cpp
+++ block-matrix-mul.cpp
@@ -18,19 +18,14 @@ struct dim2 {
 };
 
 /// Swap x and y.
-dim2 Invert(const dim2& d) {
-    return {d.y, d.x};
-}
+dim2 Invert(const dim2& d) { return {d.y, d.x}; }
 
 /// Compute dimension of matrix resulting from multiplying two matrices
 /// of sizes b1 and b1.
-dim2 DimMul(const dim2& b1, const dim2& b2) {
-    return {b1.y, b2.x};
-}
+dim2 DimMul(const dim2& b1, const dim2& b2) { return {b1.y, b2.x}; }
 
 /// Print matrix
-void Print(const dim2& size, const Float* m)
-{
+void Print(const dim2& size, const Float* m) {
     for (size_t row = 0; row != size.y; ++row) {
         for (size_t col = 0; col != size.x; ++col)
             cout << m[row * size.x + col] << " ";
@@ -54,10 +49,9 @@ void Print(const dim2& size, const Float* m)
 /// @param offsetA location of input sub-matrix inside A
 /// @param offsetB location of input sub-batrix inside B
 /// @param offsetB location of output sub-matrix inside C
-void BlockMul(const Float* a, const Float* b, Float* c, dim2 blockDimA, 
-              dim2 blockDimB, dim2 dimA, dim2 dimB, dim2 dimC,
-              dim2 offsetA, dim2 offsetB, dim2 offsetC)
-{
+void BlockMul(const Float* a, const Float* b, Float* c, dim2 blockDimA,
+              dim2 blockDimB, dim2 dimA, dim2 dimB, dim2 dimC, dim2 offsetA,
+              dim2 offsetB, dim2 offsetC) {
     const size_t nAcols = dimA.x;
     const size_t nArows = dimA.y;
     const size_t nBcols = dimB.x;
@@ -66,12 +60,11 @@ void BlockMul(const Float* a, const Float* b, Float* c, dim2 blockDimA,
     const size_t nABlockCols = blockDimB.x;
     const size_t nBBlockCols = blockDimB.x;
     const size_t nCcols = dimC.x;
-    assert(nAcols == nBrows);
     for (size_t row = 0; row != nABlockRows; ++row) {
         for (size_t col = 0; col != nBBlockCols; ++col) {
             Float v = Float(0);
             for (size_t x = 0; x != nABlockCols; ++x) {
-                v += a[(row + offsetA.y) * nAcols + offsetA.x + x] * 
+                v += a[(row + offsetA.y) * nAcols + offsetA.x + x] *
                      b[(x + offsetB.y) * nBcols + offsetB.x + col];
             }
             c[(row + offsetC.y) * nCcols + offsetC.x + col] = v;
@@ -88,9 +81,8 @@ void BlockMul(const Float* a, const Float* b, Float* c, dim2 blockDimA,
 /// @param blockDimA dimensions or A sub-matrix == dimensions of C sub-matrix
 /// @param offsetA location of sub-matrix in matrix A
 /// @param offsetC location of sub-matrix in matrix C
-void InplaceMatAdd(const Float* a, Float* c, dim2 dimA, 
-                   dim2 dimC, dim2 blockDimA, dim2 offsetA, dim2 offsetC)
-{
+void InplaceMatAdd(const Float* a, Float* c, dim2 dimA, dim2 dimC,
+                   dim2 blockDimA, dim2 offsetA, dim2 offsetC) {
     const size_t nAcols = dimA.x;
     const size_t nArows = dimA.y;
     const size_t nABlockRows = blockDimA.y;
@@ -99,12 +91,11 @@ void InplaceMatAdd(const Float* a, Float* c, dim2 dimA,
     const size_t nCrows = dimC.y;
     for (size_t row = 0; row != nABlockRows; ++row) {
         for (size_t col = 0; col != nABlockCols; ++col) {
-            c[(offsetC.y + row) * nCcols + offsetC.x + col] += 
-            a[(offsetA.y + row) * nAcols + offsetA.x + col];
+            c[(offsetC.y + row) * nCcols + offsetC.x + col] +=
+                a[(offsetA.y + row) * nAcols + offsetA.x + col];
         }
     }
 }
-
 
 /// Generic matrix-matrix multiply algorithm, use blockDim = {1, 1}
 /// if no blocking required.
@@ -118,9 +109,8 @@ void InplaceMatAdd(const Float* a, Float* c, dim2 dimA,
 ///                 such as C blocks are square
 /// @param dimA matrix A dimensions
 /// @param dimB matrix B dimensions
-void MatMul(const Float* a, const Float* b, Float* c, Float* block, 
-            dim2 blockDim, dim2 dimA, dim2 dimB)
-{
+void MatMul(const Float* a, const Float* b, Float* c, Float* block,
+            dim2 blockDim, dim2 dimA, dim2 dimB) {
     assert(a);
     assert(b);
     assert(c);
@@ -136,18 +126,18 @@ void MatMul(const Float* a, const Float* b, Float* c, Float* block,
     const size_t nBrows = dimB.y / blockDimB.y;
     const size_t nCcols = nBcols;
     const size_t nCrows = nArows;
-    const dim2 dimC = { dimB.x, dimA.y };
-    const dim2 offsetBlock = { 0, 0 };
+    const dim2 dimC = {dimB.x, dimA.y};
+    const dim2 offsetBlock = {0, 0};
     for (size_t row = 0; row != nArows; ++row) {
         for (size_t col = 0; col != nBcols; ++col) {
-            const dim2 offsetC = { col * blockDim.x, row * blockDim.y };
+            const dim2 offsetC = {col * blockDim.x, row * blockDim.y};
             for (size_t y = 0; y != nAcols; ++y) {
                 // C[row][col] = A[row][c] x B[c][col];
-                const dim2 offsetA = { y * blockDim.x, row * blockDim.y };
-                const dim2 offsetB = { col * blockDim.x, y * blockDim.y };
-                BlockMul(a, b, block, blockDim, blockDimB, dimA, dimB, 
+                const dim2 offsetA = {y * blockDim.x, row * blockDim.y};
+                const dim2 offsetB = {col * blockDim.x, y * blockDim.y};
+                BlockMul(a, b, block, blockDim, blockDimB, dimA, dimB,
                          blockDimC, offsetA, offsetB, offsetBlock);
-                InplaceMatAdd(block, c, blockDimC, dimC, blockDimC, { 0, 0 }, 
+                InplaceMatAdd(block, c, blockDimC, dimC, blockDimC, {0, 0},
                               offsetC);
             }
         }
@@ -155,8 +145,7 @@ void MatMul(const Float* a, const Float* b, Float* c, Float* block,
 }
 
 /// Generate matrices, multiply and print.
-void Test(const dim2& size, const dim2& blockSize)
-{
+void Test(const dim2& size, const dim2& blockSize) {
     vector<Float> a(size.x * size.y, Float(1));
     vector<Float> b(size.x * size.y, Float(1));
     vector<Float> c(size.x * size.y, Float(0));
@@ -166,8 +155,7 @@ void Test(const dim2& size, const dim2& blockSize)
 }
 
 /// Invoke matrix multiply test
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 #if 0
   if(argc != 4) {
     cerr << "usage: " << argv[0] << " <num rows> <num columns> <block size>"
@@ -177,8 +165,8 @@ int main(int argc, char** argv)
   const dim2 size = {stoul(argv[2]), stoul(argv[1])};
   const dim2 blockSize = {stoul(argv[3]), stoul(argv[3])};
 #endif
-    const dim2 size = { 50, 50 };
-    const dim2 blockSize = { 5, 5 };
+    const dim2 size = {50, 50};
+    const dim2 blockSize = {5, 5};
     Test(size, blockSize);
     return 0;
 }
